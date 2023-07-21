592fab3eabec428d5185f4285042e98584f5218c
Rgascoin <roman.gascoin@gmail.com>
2023-07-21 21:37:50 +0200

    Feat: add front-end nextjs empty template

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+	return <h1 className="text-4xl font-extrabold text-center text-red-500 border-2 border-black p-5 hover:bg-gray-800 duration-150	transition rounded-md max-w-2xl hover:scale-105 ease-in-out transition-all cursor-pointer">Welcome ETH Global!</h1>;
+}
