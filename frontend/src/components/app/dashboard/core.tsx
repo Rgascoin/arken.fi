74c8ad1fdcbfe03b91c5bccc9fe2961b8968243e
Rgascoin <roman.gascoin@gmail.com>
2023-07-22 02:47:56 +0200

    Feat: Add Deposit information

--- frontend/src/components/app/dashboard/core.tsx
+++ frontend/src/components/app/dashboard/core.tsx
@@ -1,7 +1,3 @@
-import { Menu, Transition } from '@headlessui/react';
-import { ChevronRightIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
-import { Fragment, useState } from 'react';
-
 import MainPanel from './mainPanel';
 import SidePanel from './sidePanel';
 
@@ -15,16 +11,23 @@ const Core = () => {
 			<main className="lg:pr-96">
 				<header className="flex items-center justify-between border-b border-white/5 p-4 sm:p-6 lg:px-8">
 					<div className={'text-gray-400'}>
-						<h1 className="text-base font-semibold leading-7 text-white">Main Pannel</h1>
-						<h2>Specific description</h2>
+						<h1 className="text-base font-semibold leading-7 text-white">Deposit List</h1>
+						<h2>View all your deposit list</h2>
+						<button
+							className={
+								'my-3 cursor-pointer rounded-xl border border-indigo-600 px-4 py-1 text-sm text-gray-200 transition-all duration-150 ease-in-out hover:scale-105 hover:border-indigo-700 hover:bg-indigo-600'
+							}
+						>
+							+ New Stake
+						</button>
 					</div>
 				</header>
 
-				{/* Service list */}
+				{/* Main list */}
 				<MainPanel />
 			</main>
 
-			{/* Activity feed */}
+			{/* Side feed */}
 			<SidePanel />
 		</div>
 	);
--- frontend/src/components/app/dashboard/mainPanel.tsx
+++ frontend/src/components/app/dashboard/mainPanel.tsx
@@ -1,62 +1,85 @@
 import { ChevronRightIcon } from '@heroicons/react/20/solid';
 import React from 'react';
 
+import { useUserContext } from '../../../contexts/userContext';
+
+const deployments = [
+	{
+		id: 1,
+		depositName: 'Deposit #1',
+		color: 'text-green-400 bg-green-400/10',
+		description: 'on StakerZ',
+		data: 'StakerZ',
+		dataColor: 'text-indigo-400 bg-indigo-400/10 ring-indigo-400/30',
+		stats: [
+			{ name: 'Vault Balance', value: '23,533.00', unit: 'eth' },
+			{ name: 'User Balance', value: '123.00', unit: 'eth' },
+			{ name: 'Re-stake', value: '5.00', unit: 'eth' },
+		],
+	},
+];
+
 function classNames(...classes: any) {
 	return classes.filter(Boolean).join(' ');
 }
 
 const MainPanel = () => {
+	const userClient = useUserContext();
+
+	const pickedDeposit = async (depositData: unknown) => {
+		if (userClient.setPickedDeposit) userClient.setPickedDeposit(depositData);
+	};
+
 	return (
 		<>
 			<ul role="list" className="divide-y divide-white/5">
-				<li className="relative flex cursor-pointer items-center space-x-4 p-4 transition-all ease-in-out hover:bg-gray-700 sm:px-6 lg:px-8">
-					<div className="min-w-0 flex-auto">
-						<div className="flex items-center gap-x-3">
+				{deployments.map((deposit) => (
+					<li
+						key={deposit.id}
+						className="relative flex cursor-pointer items-center space-x-4 p-4 transition-all ease-in-out hover:bg-gray-700 sm:px-6 lg:px-8"
+					>
+						<div className="min-w-0 flex-auto">
+							<div className="flex items-center gap-x-3">
+								<div
+									className={
+										// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+										// @ts-ignore
+										classNames(deposit.color, 'flex-none rounded-full p-1')
+									}
+								>
+									<div className="h-2 w-2 rounded-full bg-current" />
+								</div>
+								<h2 className="min-w-0 text-sm font-semibold leading-6 text-white">
+									<button
+										onClick={async () => {
+											await pickedDeposit(deposit);
+										}}
+										className="flex gap-x-2"
+									>
+										<span className="truncate whitespace-nowrap">{deposit.depositName}</span>
+										<span className="absolute inset-0" />
+									</button>
+								</h2>
+							</div>
+							<div className="mt-3 flex items-center gap-x-2.5 text-xs leading-5 text-gray-400">
+								<p className="whitespace-nowrap">{deposit.description}</p>
+							</div>
+						</div>
+						<div className={'flex-row gap-2 md:flex'}>
 							<div
-								className={
+								className={classNames(
 									// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 									// @ts-ignore
-									classNames('text-green-500', 'flex-none rounded-full p-1')
-								}
+									deposit.dataColor,
+									'md:rounded-full flex-none py-1 px-2 text-xs font-medium ring-1 ring-inset truncate',
+								)}
 							>
-								<div className="h-2 w-2 rounded-full bg-current" />
+								{deposit.data}
 							</div>
-							<h2 className="min-w-0 text-sm font-semibold leading-6 text-white">
-								<a href="#" className="flex gap-x-2">
-									<span className="truncate whitespace-nowrap">ABCDE</span>
-									<span className="absolute inset-0" />
-								</a>
-							</h2>
 						</div>
-						<div className="mt-3 flex items-center gap-x-2.5 text-xs leading-5 text-gray-400">
-							<p className="whitespace-nowrap">aaa</p>
-						</div>
-					</div>
-					<div className={'flex-row gap-2 md:flex'}>
-						<div
-							className={classNames(
-								// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-								// @ts-ignore
-								'text-gray-400',
-								'md:rounded-full flex-none py-1 px-2 text-xs font-medium ring-1 ring-inset truncate',
-							)}
-						>
-							bbbbb
-						</div>
-						<div className={'invisible hidden text-center text-gray-400 md:visible md:flex'}>{'<>'}</div>
-						<div
-							className={classNames(
-								// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-								// @ts-ignore
-								'text-gray-400',
-								'md:rounded-full flex-none py-1 px-2 text-xs font-medium ring-1 ring-inset truncate',
-							)}
-						>
-							cccc
-						</div>
-					</div>
-					<ChevronRightIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
-				</li>
+						<ChevronRightIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+					</li>
+				))}
 			</ul>
 		</>
 	);
