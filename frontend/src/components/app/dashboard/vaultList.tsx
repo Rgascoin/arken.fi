4333612f71540851e598ff755b32849a911559a6
Rgascoin <roman.gascoin@gmail.com>
2023-07-22 17:15:09 +0200

    Feat: add a dirty way to stake on a specific vault

--- frontend/src/components/app/dashboard/vaultList.tsx
+++ frontend/src/components/app/dashboard/vaultList.tsx
@@ -1,97 +1,82 @@
 import React, {useEffect, useState} from 'react';
 import getContract from "../../../web3/toolkit/getContract";
 
-const vaults = [
-	{
-		name: 'Drake',
-		initials: 'DKRS',
-		desc: 'Total: 234,123 ETH',
-		bgColor: 'bg-red-600',
-	},
-	{
-		name: 'Cat',
-		initials: 'CT',
-		desc: 'Total: 234,123 ETH',
-		bgColor: 'bg-green-600',
-	},
-	{
-		name: 'SupraChain',
-		initials: 'SPRCH',
-		desc: 'Total: 234,123 ETH',
-		bgColor: 'bg-blue-600',
-	},
-	{
-		name: 'Irgr',
-		initials: 'Ir',
-		desc: 'Total: 234,123 ETH',
-		bgColor: 'bg-orange-600',
-	},
-	{
-		name: 'Yahourt',
-		initials: 'yht',
-		desc: 'Total: 234,123 ETH',
-		bgColor: 'bg-blue-600',
-	},
-	{
-		name: 'pitch',
-		initials: 'DCK',
-		desc: 'Total: 234,123 ETH',
-		bgColor: 'bg-orange-600',
-	},
-];
+interface VaultData {
+	address: string;
+	totalAsset: string;
+	initials: string;
+	bgColor: string;
+}
 
 function classNames(...classes: any) {
 	return classes.filter(Boolean).join(' ');
 }
 
 import factoryAbi from '../../../web3/abi/Factory.json';
+import vaultAbi from '../../../web3/abi/Vault.json';
 import {useUserContext} from "../../../contexts/userContext";
 import {JsonRpcApiProvider, JsonRpcSigner, Provider} from "ethers";
-const contractAddress = '0x60193d4AB61C2872CAA509Ee532F9Db169730A46';
+const FactoryContractAddress = '0x9155497EAE31D432C0b13dBCc0615a37f55a2c87';
 
 const VaultList = () => {
 	const userClient = useUserContext();
-	const [vaultsAddress, setVaultsAddress] = useState<string[] | undefined>()
+	const [vaults, setVaults] = useState<VaultData[] | undefined>()
 
-	const fetchVaultList = async () => {
-		const fetchedAddresses: string[] = [];
-
+	const fetchVaultList = async (): Promise<string[] | Promise<undefined>> => {
 		if (!userClient.provider || !userClient.address) {
 			console.error("Can't get signer")
 			return;
 		}
 
 		const signer = new JsonRpcSigner(userClient.provider as JsonRpcApiProvider, userClient.address)
-		const contract = getContract(contractAddress, factoryAbi, signer);
+		const contract = getContract(FactoryContractAddress, factoryAbi, signer);
 		if (!contract) {
 			console.log("Can't fetch the contract")
 			return;
 		}
 
 		// Fetching data until the array is empty
-		try {
-			let index = 0;
-			while (true) {
-				const address = await contract.vaults(index);
-				fetchedAddresses.push(address);
-				index++;
-			}
-		} catch (error) {
-			console.error("Error fetching vault:", error);
+		const addresses = await contract.getVaults();
+		return addresses;
+	}
+
+	const fetchVaultsBalance = async (vaultsAddresses: string[]) => {
+		if (!userClient.provider || !userClient.address) {
+			console.error("Can't get signer")
+			return;
 		}
-		console.log(fetchedAddresses)
+		const VaultsBalances: VaultData[] = [];
+		const signer = new JsonRpcSigner(userClient.provider as JsonRpcApiProvider, userClient.address)
+
+		await Promise.all(vaultsAddresses.map(async (vaultAddress: string) => {
+			const vaultContract = getContract(vaultAddress, vaultAbi, signer);
+			if (!vaultContract) return;
+			const totalAsset = await vaultContract.totalAssets();
+			VaultsBalances.push({address: vaultAddress, totalAsset: totalAsset.toString(), initials: vaultAddress.substring(0,3), bgColor: 'bg-red-600'})
+		}));
+		return VaultsBalances;
 	}
 
 	useEffect(() => {
-		fetchVaultList();
+		const fetch = async () => {
+			const addresses = await fetchVaultList();
+			if (!addresses) {
+				console.error('failed to get vaults addresses')
+				return;
+			}
+			const fetchedVaults = await fetchVaultsBalance(addresses);
+			setVaults(fetchedVaults)
+		}
+
+		fetch();
 	}, [])
 
 	return (
 		<div>
 			<ul role="list" className="mx-6 my-12 grid grid-cols-1 gap-5 sm:grid-cols-2 sm:gap-6 lg:grid-cols-2">
-				{vaults.map((vault) => (
+				{vaults && vaults.map((vault) => (
 					<li
-						key={vault.name}
+						key={vault.address}
 						// onClick={async () => {
 						// 	await setDataForm(project);
 						// }}
@@ -107,8 +92,8 @@ const VaultList = () => {
 						</div>
 						<div className="flex flex-1 items-center justify-between truncate rounded-r-md border-y border-r border-gray-700 bg-gray-700">
 							<div className="flex-1 truncate px-4 py-2 text-sm">
-								<div className="font-medium text-gray-200 hover:text-gray-100">{vault.name}</div>
-								<p className="truncate text-gray-400">{vault.desc}</p>
+								<div className="font-medium text-gray-200 hover:text-gray-100">{vault.address}</div>
+								<p className="truncate text-gray-400">Total asset: {vault.totalAsset}</p>
 							</div>
 						</div>
 					</li>
